09fddade14423c917338c13dcd69e394a41d126f
Raman <81064836+cybr47@users.noreply.github.com>
2026-08-29 13:04:21 +0530

    fix: redo icon with explicit adaptive layers

--- app/src/main/java/com/cybr47/instar/utils/version/VersionCheckUtility.java
+++ app/src/main/java/com/cybr47/instar/utils/version/VersionCheckUtility.java
@@ -17,7 +17,7 @@ import java.net.URL;
 
 public class VersionCheckUtility {
 
-    private static final String CURRENT_VERSION = "0.1.0-alpha03"; // Current version
+    private static final String CURRENT_VERSION = "0.1.0-alpha04"; // Current version
     private static final String VERSION_CHECK_URL = "https://raw.githubusercontent.com/cybr47/Instar/refs/heads/main/version.json"; // JSON URL
 
     public static void checkForUpdates(Context context) {
